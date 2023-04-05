e452bcebf945f236cb88e85b482ecf4c0f9e7878
HAoRAn-W <haoran2326@gmail.com>
2023-04-04 21:09:38 -0400

    script update

--- gpt_csv_batch.py
+++ gpt_csv_batch.py
@@ -17,7 +17,13 @@ domains.insert(len(domains.columns), 'result', "")
 total_count = len(domains)
 step = 200
 
-for start in range(0, total_count, step):
+prompt1 = "You are a network expert. Find out the purpose of the provided URL (choose one out of the options: tracking, marketing, advertising, analytics, CDN, static server, DNS, first-party host).  Response in JSON containing following fields: company, company_website, result. Respond in JSON only."
+
+prompt2 = "You are a network expert identifying URLs. Determine the purpose of the URL and provide the following information in a JSON format: company: the name of the company that owns the URL; company_website: the website of the company; ressult: the purpose of the domain (choose one out of the options: tracking, marketing, advertising, analytics, CDN, static server, DNS, first-party host)."
+
+times = []
+
+for start in range(1200, total_count, step):
 
     start_time = time.time()
 
@@ -31,7 +37,7 @@ for start in range(0, total_count, step):
                 messages=[
                     {
                         "role": "system",
-                        "content": "You are a network expert. Find out the purpose of the provided URL (choose one out of the options: tracking, marketing, advertising, analytics, CDN, static server, DNS, first-party host).  Response in JSON containing following fields: company, company_website, result. Respond in JSON only.",
+                        "content": prompt2,
                     },
                     {"role": "user", "content": URL},
                 ],
@@ -70,9 +76,12 @@ for start in range(0, total_count, step):
     
     
 
-    filename = "./results/answers_" + str(start) + ".csv"
+    filename = "./results/prompt2/answers_" + str(start) + ".csv"
     domains.loc[list(range(start, min(start + step, total_count)))].to_csv(filename)  # , index=False
 
     end_time = time.time()
     time_used = end_time - start_time
     print("=============TIME USED: " + str(time_used) + "=====================")
+    times.append(time_used)
+
+print(times)

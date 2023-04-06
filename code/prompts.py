c22c3b698e5bc7bceb91aa9642ca3ecdb239e32c
MahatiMadhira <93003959+MahatiMadhira@users.noreply.github.com>
2023-04-06 02:04:45 -0400

    Added Prompt 3

--- code/prompts.py
+++ code/prompts.py
@@ -2,7 +2,7 @@ prompt1 = "You are a network expert. Find out the purpose of the provided URL (c
 
 prompt2 = "You are a network expert identifying URLs. Determine the purpose of the URL and provide the following information in a JSON format: company: the name of the company that owns the URL; company_website: the website of the company; ressult: the purpose of the domain (choose one out of the options: tracking, marketing, advertising, analytics, CDN, static server, DNS, first-party host)."
 
-prompt3 = ""
+prompt3 = "As a network expert, you are tasked with analyzing URLs visited by IoT devices. Your goal is to determine the purpose of each URL and provide the following information in JSON format: company name, company website, and the purpose of the domain. Please choose one of the following purposes for each domain: tracking, marketing, advertising, analytics, CDN, static server, DNS, or first-party host. The JSON format should include the following keys: "company", "company_website", and "result"."
 
 prompt4 = ""
 
